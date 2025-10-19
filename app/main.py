a513b8eed05b40595bac25658801c34c7bc9aea3
mdemore2 <53943861+mdemore2@users.noreply.github.com>
2025-10-18 19:11:19 -0700

    init

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from query import query_all
+import json
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"]
+)
 
 @app.get("/")
 def read_root():
@@ -17,7 +24,9 @@ def read_news():
 
 @app.get('/xkcd')
 def read_xkcd():
-    pass
+    with open('data/xkcd.json', 'r') as f:
+        data = json.load(f)
+        return {'img':data['img']}
 
 
 @app.get('/surf')
