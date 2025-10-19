0317601e63bff69bca4c64ddb4a278e7559f060c
mdemore2 <53943861+mdemore2@users.noreply.github.com>
2025-10-19 13:16:38 -0700

    cleanup and added wikipedia page

--- app/main.py
+++ app/main.py
@@ -20,28 +20,33 @@ logging.basicConfig(
 def read_root():
     return {"Hello":"World"}
 
-@app.get('/wx')
-def read_wx():
-    pass
-
 @app.get('/news')
 def read_news():
     with open('data/news.json', 'r') as f:
         data = json.load(f)
-        return {'article':data['article']}
+        return data
 
 @app.get('/xkcd')
 def read_xkcd():
     with open('data/xkcd.json', 'r') as f:
         data = json.load(f)
-        return {'img':data['img']}
-
-
-@app.get('/surf')
-def read_surf():
-    pass
+        return data
+    
+@app.get('/wiki')
+def read_wiki():
+    with open('data/wikipedia.json', 'r') as f:
+        data = json.load(f)
+        return data
 
 @app.get('/refresh')
 def refresh_data():
     query_all()
     return "Ahh... refreshing :)"
+
+@app.get('/surf')
+def read_surf():
+    pass
+
+@app.get('/wx')
+def read_wx():
+    pass
--- app/query.py
+++ app/query.py
@@ -2,12 +2,17 @@ import requests
 import json
 import xml.etree.ElementTree as ET
 import logging
+from datetime import datetime
+from dotenv import load_dotenv
+import os
 
 logger = logging.getLogger(__name__)
+load_dotenv()
 
 def query_all():
     query_xkcd()
     query_news()
+    query_wikipedia()
 
 def query_xkcd():
     url = 'https://xkcd.com/info.0.json'
@@ -17,19 +22,30 @@ def query_xkcd():
         json.dump(data, f)
 
 def query_news():
-    #logger.error('TEST')
     url = "https://feeds.npr.org/1001/rss.xml"
     resp = requests.get(url)
     root = ET.fromstring(resp.content)
     channel = root.find('channel')
-    logger.error(channel)
     article = channel.find('item')
-    logger.error(article)
-    for child in article:
-        logger.error(child.tag)
     content = article.find('{http://purl.org/rss/1.0/modules/content/}encoded')
-    logger.error(content)
     data = {'article': content.text}
     with open('data/news.json', 'w+') as f:
         json.dump(data, f)
+
+def query_wikipedia():
+    today = datetime.now()
+    token = os.getenv('WIKI_API_TOKEN')
+    app_name = os.getenv('WIKI_API_APP_NAME')
+    url = f"https://api.wikimedia.org/feed/v1/wikipedia/en/featured/{today.year}/{today.month}/{today.day}"
+    headers = {
+  'Authorization': f'Bearer {token}',
+  'User-Agent': f'{app_name}'
+}
+    resp = requests.get(url, headers=headers)
+    logger.info(url)
+    logger.info(resp)
+    data = resp.json()
+    with open('data/wikipedia.json', 'w+') as f:
+        json.dump(data,f)
+
 
